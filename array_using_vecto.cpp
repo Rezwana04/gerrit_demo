a217542e01b07881562986f350bf29233192c818
rezwana <rezwana.mamata@ontariotechu.net>
2022-04-04 04:31:32 -0400

    first commit

---
+++ array_using_vecto.cpp
@@ -0,0 +1,40 @@
+//#include<iostream>
+//#include<vector>
+//
+//using namespace std;
+//
+//int main()
+//
+//{
+//	int a, n, m;
+//
+//	cout << "Enter the size of list1\n";
+//	cin >> n;
+//	cout << "Enter the size of list2\n";
+//	cin >> m;
+//
+//
+//    //declare the array using vector
+//	vector<int> list1; 
+//
+//	//Assign values to the array 
+//	for (int i = 0; i < n; i++)
+//	{
+//		cout << endl << " Input list1 element # " << i;
+//		cin >> a;
+//		list1.push_back(a);
+//	}
+//
+//	
+//	//Printing values of the array
+//	cout << "Array values" << endl;
+//
+//	for (int i = 0; i < n; i++)
+//
+//	{
+//
+//		cout << list1[i] <<" ";
+//
+//	}
+//
+//}
